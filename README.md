76f4a3925ae58f1745f21c5674258186ac975f76
David Morgan <david.morgan@bridgevine.com>
2016-03-30 11:48:48 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# SEO/SEM Organic Capture (javascript.org.js)
+
+A javascript file that when attched to a landing page which posts to the Enhanced Buyflow, will capture Search Engine Refferel with Organic terms, Except in the case of google which will capture "google(organic)". 
+
+## What does it do?
+
+	1. Capture SEO referrals and place into the lead DB.
+	2. Post data to lead qual.
+	3. Create a user cookie with keywords.
+
+## Implement:
+
+	1. attach the script to the head tag of each landing page: 
+		a. <script type="text/javascript" src="<?php echo $config['siteRoot']; ?>versions/<?php echo $version; ?>/assets/js/javascript.org.js"></script>
+		b. this is not dependent upon jquery or similar framework.
+        
+	2. add script call to ajax data array in qualify.js:
+		a. getkeywords() added to array at leadOptional, example:
+        
+        $.ajax({
+		url:strRoot+'lcs.php',
+		type:'POST',
+		async:false,
+		data:{
+			'leadSourceID':form.txtPartnerID.value,
+			'sitePromoID':form.PromoID.value,
+			'useOTS':false,
+			'leadAddress1':form.txtHomeStreetAddress1.value,
+			'leadAddress2':form.txtHomeStreetAddress2.value,
+			'leadZip':form.txtHomeZip.value,
+			'siteAttributionTrackingURL':form.DigitalLandingUrl.value,
+			'sitePhoneNumber':form.strPromo1.value,
+			'urlParams':window.location.search,
+			'referringSite':window.location.host,
+			'leadStep':'qual',
+####		'leadOptional':getkeywords()
+		},
---
+++ javascript.org.js
@@ -0,0 +1,93 @@
+//custom script for measuring SEO via lead capture (LCS) onto new Enhanced Buyflow. Creates cookie of ORGANIC referring search engine with keywords. Bypasses SEM campaigns.
+//author: dmorgan 3-30-2016
+
+function getSemParams(sParam){
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}  
+function gup(name) {
+  name = name.replace(/[[]/,"\[").replace(/[]]/,"\]");
+  var regexS = "[\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( document.referrer );
+  if( results == null )
+    return "";
+  else
+    return results[1];
+}
+
+function getkeywords() {
+  var x = document.referrer;
+  var keywords = 0;
+  if(x == null || x == 0){
+	  keyword="Direct";
+	  return keyword;
+  }
+  if(getSemParams("PID") || getSemParams("pid") || getSemParams("gclid")){
+	  keywords="SEM (pid/gclid) String";
+	  return keywords;
+  }else{	  
+	  if (x.search(/yahoo/) != -1) {
+		keywords = 'yahoo+'+gup("p"); 
+	  }
+	  else if (x.search(/digg/) != -1) {
+		keywords = 'digg+'+gup("s"); 
+	  }
+	  else {
+		var referrer =  x.match(/:\/\/(.[^/]+)/)[1];
+		if(referrer.indexOf("google")!=-1){
+			keywords="google(organic)";
+		}else{
+			keywords = referrer+':'+gup("q"); 
+		}
+	  }
+
+  }
+  return keywords;  
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function checkKeywordsExist() {
+    var go=getCookie("organic");
+	var x = document.referrer;
+	var z = window.location.href;
+	if(x == null || x ==0){
+		setCookie("organic", "Direct", .0115);
+		return false;
+	}
+	var internalpages = z.match(/:\/\/(.[^/]+)/)[1];
+	var referringpages = x.match(/:\/\/(.[^/]+)/)[1];
+	if(go==""){
+		setCookie("organic", getkeywords(), .0115);
+	}
+		if(internalpages!=referringpages){
+			setCookie("organic", getCookie("organic"), .0115);
+		}
+		if(referringpages.indexOf(getCookie("organic"))!=-1){
+			setCookie("organic", getkeywords(), .0115);
+		}
+}
+checkKeywordsExist();
